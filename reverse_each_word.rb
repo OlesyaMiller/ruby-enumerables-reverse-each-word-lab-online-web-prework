5beff5d6cb85343c0b8fb00b454c848fbd823237
OLESYA MILLER <olesyam.miller@gmail.com>
2019-12-02 02:53:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -14,12 +14,5 @@ end
   #reversed_words.join(" ")
 #end
 
-def reverse_each_word(string)
-   
-  test_array = []
-  string.split(" ").collect do |word| 
-    test_array << word.reverse 
-  end
-  test_array.join(" ")
-end
+
 
