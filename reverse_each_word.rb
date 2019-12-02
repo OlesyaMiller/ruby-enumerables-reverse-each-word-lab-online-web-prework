b3da82fbf41294a21e8ef21e16d52f4888d415b4
OLESYA MILLER <olesyam.miller@gmail.com>
2019-12-02 02:53:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,19 +6,20 @@ def reverse_each_word(string_sentence)
   reversed_words.join(" ")
 end
 
-#def reverse_each_words(string_sentence)
+#def reverse_each_word(string)
  # reversed_words = []
-  #string_sentence.split(" ").collect do |word|
+  #string.split(" ").collect do |word|
    # reversed_words << word.reverse
   #end
   #reversed_words.join(" ")
 #end
 
 def reverse_each_word(string)
-  array = string.split(" ") #turn string into an array
+   
   test_array = []
-  array.collect do|string| #iterate over the array
-    test_array << string.reverse #reverse each word in the array
+  string.split(" ").collect do |word| 
+    test_array << word.reverse 
   end
   test_array.join(" ")
 end
+
